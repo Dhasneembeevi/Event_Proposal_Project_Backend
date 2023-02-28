be5eb3824d9f779b937e856685f81567055604e0
Dhasneembeevi <dhasmohamed1020@gmail.com>
2023-02-28 15:36:20 +0530

    added get api

---
+++ Routes/Proposal.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Event } = require('../models/proposal')
+
+
+router.get('/allproposals', async(req,res)=>{
+   // const { proposalType, eventType,  budget, fromDate, toDate} = req.body;
+    try {
+        const proposals = await Event.find().sort({_id:"-1"})
+        res.json({
+            status: "success",
+            proposals,
+        })
+    } 
+    catch (error) {
+        res.status(404).json({
+            status: "Failed",
+            message: error.message
+        })   
+    }
+})
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express=require('express');
-const cors = require('cors');
-
+const express = require ("express");
 const app = express();
-
+const dotenv = require("dotenv");
+// const userRoute = require("./routes/user");
+// const authRoute = require("./routes/auth")
+dotenv.config();
+const mongoose = require("mongoose");
+const allProposals = require('./Routes/Proposal')
+mongoose.set('strictQuery', false);
 app.use(express.json());
+
+const PORT = 5000 || process.env.PORT
+
+mongoose.connect(process.env.MONGO_URL).then(()=>
+    console.log("connected to mongodb")
+).catch((err )=>{
+    console.log(`${err}`)
+})
 
 app.get('/', (req,res)=>{
     res.send("hello")
 })
 
-app.listen(5000, () =>{
-    console.log("port is running at 5000")
+app.use('/api', allProposals)
+
+app.listen(PORT, () =>{
+    console.log(`port is running at ${PORT}`)
 })
